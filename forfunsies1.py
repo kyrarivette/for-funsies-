4842a0a8c41c166df21c0b168fc147b7252d0e08
Kyra <spis16bf@its-cseb230-21.ucsd.edu>
2016-08-11 21:57:01 -0700

    random

---
+++ forfunsies1.py
@@ -0,0 +1,9 @@
+'''for funsies coding'''
+import turtle
+turtle=Turtle()
+steve=turtle.clone()
+def clonetime():
+    turtle.forward (50)
+    turtle.clone()
+    turtle.angle(45)
+    turtle.forward(25)
